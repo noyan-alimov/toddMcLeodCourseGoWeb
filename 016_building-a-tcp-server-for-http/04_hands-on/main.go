3f34c6c759d07405e0f651c2f0de1632b42edfcc
Noyan Alimov <noyan.alimov@gmail.com>
2020-11-08 22:32:30 +0000

    learned more about tcp and servers

---
+++ 016_building-a-tcp-server-for-http/04_hands-on/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Fatalln(err.Error())
+			continue
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	// read request
+	request(conn)
+
+	// write response
+	respond(conn)
+}
+
+func request(conn net.Conn) {
+	i := 0
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if i == 0 {
+			// request line
+			u := strings.Fields(ln)[1]
+			mux(conn, u)
+		}
+		if ln == "" {
+			// headers are done
+			break
+		}
+		i++
+	}
+}
+
+func mux(conn net.Conn, u string) {
+	if u == "/hello" {
+		hello(conn)
+	}
+	if u == "/goodbye" {
+		goodbye(conn)
+	}
+}
+
+func hello(conn net.Conn) {
+	body := `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body><strong>Hello !</strong></body></html>`
+
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
+}
+
+func goodbye(conn net.Conn) {
+	body := `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body><strong>Good Bye !</strong></body></html>`
+
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
+}
+
+func respond(conn net.Conn) {
+
+	body := `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body><strong>Hello World</strong></body></html>`
+
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
+}

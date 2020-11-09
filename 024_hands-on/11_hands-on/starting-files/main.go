2e6072e4e87f0332fe91066b5dc2b1556b18b0ad
Noyan Alimov <noyan.alimov@gmail.com>
2020-11-09 22:04:48 +0000

    small change

--- 024_hands-on/11_hands-on/starting-files/main.go
+++ 024_hands-on/11_hands-on/starting-files/main.go
@@ -23,17 +23,17 @@ func main() {
 
 func index(w http.ResponseWriter, req *http.Request) {
 	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
-	HandleError(w, err)
+	handleError(w, err)
 }
 
 func about(w http.ResponseWriter, req *http.Request) {
 	err := tpl.ExecuteTemplate(w, "about.gohtml", nil)
-	HandleError(w, err)
+	handleError(w, err)
 }
 
 func contact(w http.ResponseWriter, req *http.Request) {
 	err := tpl.ExecuteTemplate(w, "contact.gohtml", nil)
-	HandleError(w, err)
+	handleError(w, err)
 }
 
 func apply(w http.ResponseWriter, req *http.Request) {
@@ -43,15 +43,15 @@ func apply(w http.ResponseWriter, req *http.Request) {
 	}
 
 	err := tpl.ExecuteTemplate(w, "apply.gohtml", nil)
-	HandleError(w, err)
+	handleError(w, err)
 }
 
 func applyProcess(w http.ResponseWriter, req *http.Request) {
 	err := tpl.ExecuteTemplate(w, "applyProcess.gohtml", nil)
-	HandleError(w, err)
+	handleError(w, err)
 }
 
-func HandleError(w http.ResponseWriter, err error) {
+func handleError(w http.ResponseWriter, err error) {
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		log.Fatalln(err)
